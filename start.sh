6044596afefaa16f71d652c8ec92f220acd33bdd
angrybat <jasonmoore95@hotmail.com>
2025-10-25 19:26:36 +0100

    Updated the start.sh script to only expose the charge-guru application

--- start.sh
+++ start.sh
@@ -4,14 +4,18 @@
 Xvfb :1 -screen 0 ${VNC_RESOLUTION}x24 &
 sleep 2
 
-# Start desktop environment
-xfce4-session &
+# Start minimal window manager (optional)
+matchbox-window-manager -use_titlebar no &
+
+# Start ChargeGuru in full-screen mode (if supported)
+DISPLAY=:1 ChargeGuru &
+
+# Wait a bit to ensure app is running before starting VNC
+sleep 3
 
 # Start VNC server
-x11vnc -display :1 -nopw -forever -listen 0.0.0.0 -rfbport ${VNC_PORT} &
+x11vnc -display :1 -nopw -forever -listen 0.0.0.0 -rfbport ${VNC_PORT} \
+        -noxrecord -noxfixes -noxdamage -clip ${VNC_RESOLUTION}x${VNC_RESOLUTION} &
 
-# Start noVNC (Websockify)
-websockify --web=/usr/share/novnc/ ${NOVNC_PORT} localhost:${VNC_PORT} &
-
-# Start ChargeGuru 
-ChargeGuru
+# Start noVNC
+websockify --web=/usr/share/novnc/ ${NOVNC_PORT} localhost:${VNC_PORT}
