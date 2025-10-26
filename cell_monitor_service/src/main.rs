d48bd0585b811f4f828471dbf7d809e90bc5bfb3
angrybat <jasonmoore95@hotmail.com>
2025-10-26 16:06:52 +0000

    Added code to detect if the charge is connected at startup, this will print a message to the console if it is connected

--- cell_monitor_service/src/main.rs
+++ cell_monitor_service/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use udev::{Enumerator};
+use std::io;
+
+const SKYRC_B6AC_V2_PRODUCT: &str = "C8051F3xx Development Board";
+
+fn main() -> io::Result<()> {
+    let mut enumerator = Enumerator::new()?;
+    enumerator.match_subsystem("usb")?;
+    for device in enumerator.scan_devices()? {
+        let is_skyrc_b6ac_v2 = match device.attribute_value("product").map_or(None, |f| f.to_str()) {
+            Some(n) => n == SKYRC_B6AC_V2_PRODUCT,
+            None => false,
+        };
+        if is_skyrc_b6ac_v2 {
+            println!("SkyRC B6AC V2 connected at {:?}", device.syspath());
+        }
+    }
+    return Ok(())
 }
