9ac5a713cd1957fd0ec9347d9ba17f884f27f2dd
angrybat <jasonmoore95@hotmail.com>
2025-10-26 14:01:02 +0000

    Added logic to install dependencies for the project in the post create command

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install Dependencies
+sudo apt-get update -y
+sudo apt-get install -y libudev-dev pkg-config usbutils
+
+# Install Dagger CLI
 curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.19.2 BIN_DIR=$HOME/.local/bin sh
 mkdir -p /home/vscode/.local/share/bash-completion/completions
 dagger completion bash > /home/vscode/.local/share/bash-completion/completions/dagger
